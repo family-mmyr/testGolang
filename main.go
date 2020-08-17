a149760e6cea86c0c2bfe7b23bc08090b90bb384
family-mmyr <family.mmyr@gmail.com>
2020-08-17 11:58:15 +0900

    feat: 環境変数で切り替えれるように変更

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
-	log "github.com/sirupsen/logrus"
+	"os"
+
+	"github.com/sirupsen/logrus"
 )
 
+func init() {
+	p := os.Getenv("PHASE")
+
+	if p == "DEBUG" {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		// デフォルトはテキスト形式のため、JSONフォーマットに切替える
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+
+		// Warn以上ののログを出力する
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
+}
+
 func main() {
-	log.WithFields(
-		log.Fields{
-			"animal": "dog"
-			}).Info("hello")
+	logrus.WithFields(logrus.Fields{
+		"log_version": "v1",
+		"request_id":  "aaa",
+	}).Info("operation")
+
+	logrus.WithFields(logrus.Fields{
+		"log_version": "v1",
+		"request_id":  "aaa",
+	}).Debug("debug")
 }
